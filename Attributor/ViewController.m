87709d0c3d4eeccdd202c839cbbca5704f7405a7
Kelsey Pedersen <kelseypedersen@gmail.com>
2015-07-13 12:50:25 -0700

    Add viewController lifecycle methods to maintain outline when leave app and come back

--- Attributor/ViewController.m
+++ Attributor/ViewController.m
@@ -11,11 +11,55 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextView *body;
 @property (weak, nonatomic) IBOutlet UILabel *headLine;
+@property (weak, nonatomic) IBOutlet UIButton *outlineButton;
 
 
 @end
 
 @implementation ViewController
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSMutableAttributedString *title =
+        [[NSMutableAttributedString alloc]initWithString:self.outlineButton.currentTitle];
+    [title setAttributes:@{ NSStrokeWidthAttributeName : @3,
+                            NSStrokeColorAttributeName : self.outlineButton.tintColor }
+                   range: NSMakeRange(0, [title length])];
+    [self.outlineButton setAttributedTitle:title forState:UIControlStateNormal];
+     
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self usePreferredFonts];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(preferredFontChanged:)
+                                                 name:UIContentSizeCategoryDidChangeNotification
+                                               object:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIContentSizeCategoryDidChangeNotification object:nil];
+}
+
+-(void)preferredFontChanged: (NSNotification *)notification
+{
+    [self usePreferredFonts];
+}
+
+-(void)usePreferredFonts
+{
+    self.body.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    // Is this headline or headLine?
+    self.headLine.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+}
+
+
+
 - (IBAction)changeBodySelectorColorToMatchBackgroundOfButton:(UIButton *)sender {
     
     [self.body.textStorage addAttribute:NSForegroundColorAttributeName value:sender.backgroundColor range:self.body.selectedRange];
@@ -43,10 +87,6 @@
 
 
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
