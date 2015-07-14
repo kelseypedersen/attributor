b7765d64496ca34e2fb8c879bb1123c06899aa6c
Kelsey Pedersen <kelseypedersen@gmail.com>
2015-07-13 19:07:56 -0700

    Add stats MVC

---
+++ Attributor/TextStatsViewController.h
@@ -0,0 +1,15 @@
+//
+//  TextStatsViewController.h
+//  Attributor
+//
+//  Created by Kelsey Pedersen on 7/13/15.
+//  Copyright (c) 2015 Kelsey Pedersen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextStatsViewController : UIViewController
+
+@property(nonatomic, strong)NSAttributedString *textToAnalyze;
+
+@end
